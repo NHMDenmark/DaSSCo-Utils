9bb64992b54db9672530a24a0d27c39bbda52290
Lars Dam <larsphdam@gmail.com>
2025-03-20 12:10:22 +0100

    update metadata version to 3.0.1, update test and models to 3.0.1

--- dassco_utils/metadata/models.py
+++ dassco_utils/metadata/models.py
@@ -2,6 +2,18 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import List, Dict, Optional
 
+class IssueModel(BaseModel):
+    name: str
+    timestamp: Optional[datetime] = None
+    description: Optional[str] = None
+    note: Optional[str] = None
+    solved: bool = False
+
+class LegalityModel(BaseModel):
+    copyright: Optional[str] = None
+    license: Optional[str] = None
+    credit: Optional[str] = None
+
 class MetadataModel(BaseModel):
     asset_created_by: Optional[str] = None
     asset_deleted_by: Optional[str] = None
@@ -30,12 +42,13 @@ class MetadataModel(BaseModel):
     file_format: Optional[str] = None
     funding: List[str] = []
     institution: str
-    issues: List[str] = []
+    issues: List[IssueModel] = []
+    legality: LegalityModel = LegalityModel()
     make_public: bool = False
     metadata_created_by: Optional[str] = None
     metadata_source: Optional[str] = None
     metadata_updated_by: Optional[str] = None
-    metadata_version: Optional[str] = "v3.0.0"
+    metadata_version: Optional[str] = "v3.0.1"
     mos_id: Optional[str] = None
     multi_specimen: bool = False
     parent_guid: Optional[str] = None
