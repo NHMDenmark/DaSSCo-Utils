f5ebccfb5c7e3b1773e86b5f0e9c2d468fa757b6
khaledzamzam <khaled-yahia-zamzam@hotmail.com>
2025-04-11 15:49:20 +0200

    add pika to installation requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['pydantic'],
+    install_requires=['pydantic', 'pika'],
     python_requires=">=3.10",
     classifiers=[
         'Intended Audience :: Developers',
