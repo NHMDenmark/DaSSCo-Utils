f0168596a635848fadb04e3938eaa22c8acc8a14
k-zamzam <wdm563@ku.dk>
2025-08-11 10:51:19 +0200

    update consumer example

--- examples/example_consumers.py
+++ examples/example_consumers.py
@@ -2,12 +2,12 @@ from time import sleep
 
 from dassco_utils.messaging import RabbitMqClient
 
-def handler_one(msg):
+def handler_one(msg, props):
     print('Handler One Running')
     sleep(5)
     print('Handler One Completed')
 
-def handler_two(msg):
+def handler_two(msg, props):
     print('Handler Two Running')
     sleep(2)
     print('Handler Two Completed')
@@ -20,6 +20,3 @@ client.publish('test_queue2', 'Hello World')
 client.add_handler('test_queue', handler_one)
 client.add_handler('test_queue2', handler_two)
 client.start_consuming()
-sleep(10)
-client.stop_consuming()
-
