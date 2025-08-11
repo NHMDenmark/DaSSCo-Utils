183294775a70156d14de514959fbd4e2b3909310
k-zamzam <wdm563@ku.dk>
2025-08-11 10:47:44 +0200

    update metadata structure

---
+++ examples/example_consumer.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+from dassco_utils.messaging import RabbitMqClient
+
+def handler_one(msg):
+    print('Handler One Running')
+    sleep(5)
+    print('Handler One Completed')
+
+def handler_two(msg):
+    print('Handler Two Running')
+    sleep(2)
+    print('Handler Two Completed')
+
+client = RabbitMqClient(run_async=True)
+
+client.publish('test_queue', 'Hello World')
+client.publish('test_queue2', 'Hello World')
+
+client.add_handler('test_queue', handler_one)
+client.add_handler('test_queue2', handler_two)
+client.start_consuming()
+sleep(10)
+client.stop_consuming()
+
